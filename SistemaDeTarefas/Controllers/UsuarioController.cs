700d2e7f68c47f952c2cf5ac9537cdf8928f37f2
Marcos José <marcosjos15@hotmail.com>
2023-04-05 15:41:22 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        //Metodo para buscar usuários
+        [HttpGet]
+
+        //BuscarTodosUsuarios - Nome do nosso EndPoint
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SistemaDeTarefas/Data/SistemaTarefaDBConex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    //Herdar de :
+    public class SistemaTarefaDBContex : DbContext
+    {
+        //ctro - Criar um Construtor
+        public SistemaTarefaDBContex(DbContextOptions<SistemaTarefaDBContex> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
